0e113d61e605a8bdef55f1d001a0375a60c860cf
Robert Keffury <bobby.keffury@me.com>
2020-09-17 13:30:31 -0700

    Work in Progress

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -1,45 +1,53 @@
-"""
-Simple graph implementation
-"""
 from util import Stack, Queue  # These may come in handy
 
 class Graph:
 
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        pass  # TODO
+        if v1 and v2 in self.vertices:
+            self.vertices[v1].add(v2)
 
     def get_neighbors(self, vertex_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
-        """
-        Print each vertex in breadth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        queue = Queue()
+        queue.enqueue(starting_vertex)
+
+        visited = set()
+
+        while queue.size() > 0:
+            current_vertex = queue.dequeue()
+
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+            
+            neighbors = self.get_neighbors(current_vertex)
+
+            for neighbor in neighbors:
+                queue.enqueue(neighbor)
 
     def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        pass  # TODO
+        stack = Stack()
+        stack.push(starting_vertex)
+
+        visited = set()
+
+        while stack.size() > 0:
+            current_vertex = stack.pop()
+
+            if current_vertex not in visited:
+                visited.add(current_vertex)
+
+            neighbors = self.get_neighbors(current_vertex)
+
+            for neighbor in neighbors:
+                stack.push(neighbor)
 
     def dft_recursive(self, starting_vertex):
         """
